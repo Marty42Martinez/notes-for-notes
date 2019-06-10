f5045248d4175475c0014d9cf2d441ca23b387fa
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-10 10:36:19 -0700

    reducer handles pending state

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,9 +1,16 @@
+import { NEW_NOTE_PENDING } from '../actions/noteActions';
+
 const initialState = {
   loading: false,
   list: []
 };
 
 export default function reducer(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case NEW_NOTE_PENDING:
+      return { ...state, loading: true };
+    default:
+      return state
+  }
 }
 
