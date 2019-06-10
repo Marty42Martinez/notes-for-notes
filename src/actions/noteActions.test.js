cdd6333ec4eb2550950a83774ba00e741041b4fa
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-10 11:02:18 -0700

    added mock call in actions test. previously forgot

--- src/actions/noteActions.test.js
+++ src/actions/noteActions.test.js
@@ -1,5 +1,11 @@
 import { newNote } from './noteActions';
 
+jest.mock('../services/notesApi.js', () => ({
+  createNote() {
+    return Promise.resolve([]);
+  }
+}));
+
 describe('noteAction creator tests', () => {
   it('dispatches 4 actions when newNote is invoked', () => {
 
