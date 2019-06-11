7a9ed901aa724fca5549b5e63d6b4d2e8b2b5538
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-11 14:38:03 -0700

    Routes added to App, Home and Callback

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import CreateNote from '../containers/notes/CreateNote';
-import GatherNotes from '../containers/notes/GatherNotes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Callback from '../containers/auth/Callback';
+import { withSession } from '../containers/auth/withSession';
 
 export default function App() {
   return (
-    <>
-      <CreateNote />
-      <GatherNotes />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={withSession(Home)} />
+        <Route path="/callback" component={Callback} />
+      </Switch>
+    </Router>
   );
 }
   
