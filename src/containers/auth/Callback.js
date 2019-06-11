632e76e8b9f519e373151184393ab71468ffa103
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-11 14:51:21 -0700

    Callback connected to Redux Store

--- src/containers/auth/Callback.js
+++ src/containers/auth/Callback.js
@@ -3,16 +3,31 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { setSession } from '../../actions/sessionActions';
 
-export default class Callback extends PureComponent {
+class Callback extends PureComponent {
   static propTypes = {
-
+    handleAuth: PropTypes.func.isRequired
   }
 
   componentDidMount() {
-
+    this.props.handleAuth();
   }
 
   render() {
-    return <h1>CALLBACK</h1>;
+    return <h1></h1>;
   }
 }
+
+const mapDispatchToProps = (dispatch, props) => ({
+  handleAuth() {
+    const action = setSession();
+    dispatch(action);
+    action.payload.then(() => {
+      props.history.push('/');
+    });
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Callback);
