13d551e43655be7fe0cda7a799746b007bb02ded
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-11 14:47:31 -0700

    export callback to properly access component

--- src/containers/auth/Callback.js
+++ src/containers/auth/Callback.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { setSession } from '../../actions/sessionActions';
 
-class Callback extends PureComponent {
+export default class Callback extends PureComponent {
   static propTypes = {
 
   }
